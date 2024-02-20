41fb25f34e710bcdbf1e7c89bdd49de6394c7c50
MaksimSinicbIn <dzigen1986@gmail.com>
2024-02-20 23:28:16 +0700

    Refactoring filter function

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import { v1 } from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
 
+export function getFilteredTasks (allTasks: Array<TaskType>, filterValue: FilterValuesType):Array<TaskType> {
+    let tasksForTodolist = allTasks;
+    if (filterValue === "active") {
+        tasksForTodolist = allTasks.filter(t => t.isDone === false);
+    }
+    if (filterValue === "completed") {
+        tasksForTodolist = allTasks.filter(t => t.isDone === true);
+    }
+    return tasksForTodolist
+}
+
 function App() {
     //BLL
     let [tasks, setTasks] = useState<Array<TaskType>>([
@@ -36,22 +47,29 @@ function App() {
         setFilter(value);
     }
 
-    let tasksForTodolist = tasks;
+    function changeTaskStatus (id: string, newIsDone: boolean) {
+        // const task = tasks.find( t => t.id === id) // TaskType || undefined
+        // if (task) {
+        //     task.isDone = !task.isDone // меняем статус мутабельно
+        //     setTasks([...tasks])
+        // }
+        const nextState = tasks.map(t => t.id === id ? {...t, isDone: newIsDone} : t)
+        setTasks(nextState)
+    }
 
-    if (filter === "active") {
-        tasksForTodolist = tasks.filter(t => t.isDone === false);
-    }
-    if (filter === "completed") {
-        tasksForTodolist = tasks.filter(t => t.isDone === true);
-    }
     // UI
+    const filteredTasks = getFilteredTasks(tasks, filter)
+    
     return (
         <div className="App">
             <Todolist title="What to learn"
-                        tasks={tasksForTodolist}
+                        tasks={filteredTasks}
+                        filter={filter}
                         removeTask={removeTask}
                         changeFilter={changeFilter} 
-                        addTask={addTask}/>
+                        addTask={addTask}
+                        changeTaskStatus={changeTaskStatus}
+            />
         </div>
     );
 }
