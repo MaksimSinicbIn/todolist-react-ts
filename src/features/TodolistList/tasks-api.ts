58fec7dc105af4c39773e9cf12c4fbf99433aaca
MaksimSinicbIn <dzigen1986@gmail.com>
2024-06-06 00:21:58 +0700

    Fix tasks-api's request paths

--- src/features/TodolistList/tasks-api.ts
+++ src/features/TodolistList/tasks-api.ts
@@ -48,16 +48,16 @@ export type ArgsRemoveTaskType = {
 
 export const TasksApi = {
     getTasks (todolistId: string) {
-        return instance.get<GetTasksResponse>(`${todolistId}/tasks`)
+        return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
     },
     createTasks (arg: ArgsAddType) {
         const { todolistId, title } = arg
-        return instance.post<BaseResponseType<{ item: TaskType}>>(`${todolistId}/tasks`, {title})
+        return instance.post<BaseResponseType<{ item: TaskType}>>(`todo-lists/${todolistId}/tasks`, {title})
     },
     deleteTasks (todolistId: string, taskId: string) {
-        return instance.delete<BaseResponseType>(`${todolistId}/tasks/${taskId}`)
+        return instance.delete<BaseResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`)
     },
     updateTasks (todolistId: string, taskId: string, model: UpdateTaskModelType) {
-        return instance.put<BaseResponseType>(`${todolistId}/tasks/${taskId}`, model)
+        return instance.put<BaseResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`, model)
     }
 }
