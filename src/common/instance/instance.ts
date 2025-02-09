60a12803227e76d08309bec762e2abfc56066bf5
MaksimSinicbIn <dzigen1986@gmail.com>
2025-02-09 17:28:24 +0700

    Add Environment Variables + refactoring instance component

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
     headers: {
-        'API-KEY': '2d27c019-1d06-4714-83cd-3d363de42b42'
+        Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
+        'API-KEY': process.env.REACT_APP_API_KEY
     }
 })
